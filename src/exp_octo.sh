e85b610d4c57c4384c59f9996df9293cec6714d7
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-11-03 21:08:46 +0900

    Anomaly Detection

--- src/exp_octo.sh
+++ src/exp_octo.sh
@@ -1,34 +1,26 @@
 #!/bin/sh
 
-export CUDA_VISIBLE_DEVICES=0
-python3 run_exp.py --conf_file_path ./config/IC_PN_BEATS/gnn.yaml &
-sleep 3
+
 
 export CUDA_VISIBLE_DEVICES=1
-python3 run_exp.py --conf_file_path ./config/IC_PN_BEATS/none_gnn.yaml &
+python3 run_exp.py --conf_file_path ./config/N_BEATS_G.yaml &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=2
-python3 run_exp.py --conf_file_path ./config/N_BEATS_G/gnn.yaml &
-sleep 3
+
 
 export CUDA_VISIBLE_DEVICES=3
-python3 run_exp.py --conf_file_path ./config/N_BEATS_G/none_gnn.yaml &
+python3 run_exp.py --conf_file_path ./config/N_BEATS_I.yaml &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=4
-python3 run_exp.py --conf_file_path ./config/N_BEATS_I/gnn.yaml &
-sleep 3
+
 
 export CUDA_VISIBLE_DEVICES=5
-python3 run_exp.py --conf_file_path ./config/N_BEATS_I/none_gnn.yaml &
+python3 run_exp.py --conf_file_path ./config/N_HiTS.yaml &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=6
-python3 run_exp.py --conf_file_path ./config/N_HiTS/gnn.yaml &
-sleep 3
+
 
 export CUDA_VISIBLE_DEVICES=7
-python3 run_exp.py --conf_file_path ./config/N_HiTS/none_gnn.yaml &
+python3 run_exp.py --conf_file_path ./config/IC_PN_BEATS.yaml &
 sleep 3
 
