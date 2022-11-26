abb3b2522d36d51fd59c3979a9cbd9d3674e2bc4
KimJuHyeon <wngus1310@naver.com>
2022-11-27 02:39:53 +0900

    3hour exp

--- src/exp_octo.sh
+++ src/exp_octo.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 
-export CUDA_VISIBLE_DEVICES=7
+export CUDA_VISIBLE_DEVICES=4
 python3 run_gridsearch_exp.py --conf_file_path ./config/N_BEATS.yaml &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=5
 python3 run_gridsearch_exp.py --conf_file_path ./config/N_HiTS.yaml &
 sleep 3
 
-export CUDA_VISIBLE_DEVICES=2
+export CUDA_VISIBLE_DEVICES=6
 python3 run_gridsearch_exp.py --conf_file_path ./config/IC_PN_BEATS.yaml &
 sleep 3
