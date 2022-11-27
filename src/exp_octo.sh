2586a6352c4be8f7e05514bfe64926b6a974c86f
Kim-Ju-Hyeon <wngus1310@naver.com>
2022-11-27 13:23:53 +0900

    e

--- src/exp_octo.sh
+++ src/exp_octo.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 
-export CUDA_VISIBLE_DEVICES=4
-python3 run_gridsearch_exp.py --conf_file_path ./config/N_BEATS.yaml &
-sleep 3
-
-export CUDA_VISIBLE_DEVICES=5
-python3 run_gridsearch_exp.py --conf_file_path ./config/N_HiTS.yaml &
-sleep 3
+#export CUDA_VISIBLE_DEVICES=4
+#python3 run_gridsearch_exp.py --conf_file_path ./config/N_BEATS.yaml &
+#sleep 3
+#
+#export CUDA_VISIBLE_DEVICES=5
+#python3 run_gridsearch_exp.py --conf_file_path ./config/N_HiTS.yaml &
+#sleep 3
 
 export CUDA_VISIBLE_DEVICES=6
 python3 run_gridsearch_exp.py --conf_file_path ./config/IC_PN_BEATS.yaml &
